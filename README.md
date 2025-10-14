f1e69f3f1e759b4867c06b17ce212a3b028821c8
rgarciacarmona <r.garcia.carmona@gmail.com>
2025-10-14 20:31:28 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ALED (Algorithms and Data Structures) Lab Session 3
+
+This repository contains the code for a Java application that performs a linear and a binary search to find a DNA sequence inside a reference genome.
---
+++ src/es/upm/dit/aled/lab3/binary/FASTAReaderSuffixes.java
@@ -0,0 +1,105 @@
+package es.upm.dit.aled.lab3.binary;
+
+import java.util.Arrays;
+import java.util.List;
+
+import es.upm.dit.aled.lab3.FASTAReader;
+
+/**
+ * Reads a FASTA file containing genetic information and allows for the search
+ * of specific patterns within these data. The information is stored as an array
+ * of bytes that contain nucleotides in the FASTA format. Since this array is
+ * usually created before knowing how many characters in the origin FASTA file
+ * are valid, an int indicating how many bytes of the array are valid is also
+ * stored. All valid characters will be at the beginning of the array.
+ * 
+ * This extension of the FASTAReader uses a sorted dictionary of suffixes to
+ * allow for the implementation of binary search.
+ * 
+ * @author mmiguel, rgarciacarmona
+ *
+ */
+public class FASTAReaderSuffixes extends FASTAReader {
+	protected Suffix[] suffixes;
+
+	/**
+	 * Creates a new FASTAReader from a FASTA file.
+	 * 
+	 * At the end of the constructor, the data is sorted through an array of
+	 * suffixes.
+	 * 
+	 * @param fileName The name of the FASTA file.
+	 */
+	public FASTAReaderSuffixes(String fileName) {
+		// Calls the parent constructor
+		super(fileName);
+		this.suffixes = new Suffix[validBytes];
+		for (int i = 0; i < validBytes; i++)
+			suffixes[i] = new Suffix(i);
+		// Sorts the data
+		sort();
+	}
+
+	/*
+	 * Helper method that creates a array of integers that contains the positions of
+	 * all suffixes, sorted alphabetically by the suffix.
+	 */
+	private void sort() {
+		// Instantiate the external SuffixComparator, passing 'this' (the reader)
+		// so it can access the content and validBytes fields.
+		SuffixComparator suffixComparator = new SuffixComparator(this);
+		// Use the external Comparator for sorting.
+		Arrays.sort(this.suffixes, suffixComparator);
+	}
+
+	/**
+	 * Prints a list of all the suffixes and their position in the data array.
+	 */
+	public void printSuffixes() {
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("Index | Sequence");
+		System.out.println("-------------------------------------------------------------------------");
+		for (int i = 0; i < suffixes.length; i++) {
+			int index = suffixes[i].suffixIndex;
+			String ith = "\"" + new String(content, index, Math.min(50, validBytes - index)) + "\"";
+			System.out.printf("  %3d | %s\n", index, ith);
+		}
+		System.out.println("-------------------------------------------------------------------------");
+	}
+
+	/**
+	 * Implements a binary search to look for the provided pattern in the data
+	 * array. Returns a List of Integers that point to the initial positions of all
+	 * the occurrences of the pattern in the data.
+	 * 
+	 * @param pattern The pattern to be found.
+	 * @return All the positions of the first character of every occurrence of the
+	 *         pattern in the data.
+	 */
+	@Override
+	public List<Integer> search(byte[] pattern) {
+		// TODO
+		return null;
+	}
+
+	public static void main(String[] args) {
+		long t1 = System.nanoTime();
+		FASTAReaderSuffixes reader = new FASTAReaderSuffixes(args[0]);
+		if (args.length == 1)
+			return;
+		byte[] patron = args[1].getBytes();
+		System.out.println("Tiempo de apertura de fichero: " + (System.nanoTime() - t1));
+		long t2 = System.nanoTime();
+		System.out.println("Tiempo de ordenación: " + (System.nanoTime() - t2));
+		reader.printSuffixes();
+		long t3 = System.nanoTime();
+		List<Integer> posiciones = reader.search(patron);
+		System.out.println("Tiempo de búsqueda: " + (System.nanoTime() - t3));
+		if (posiciones.size() > 0) {
+			for (Integer pos : posiciones)
+				System.out.println("Encontrado " + args[1] + " en " + pos);
+		} else
+			System.out.println("No he encontrado " + args[1] + " en ningún sitio.");
+		System.out.println("Tiempo total: " + (System.nanoTime() - t1));
+	}
+}
